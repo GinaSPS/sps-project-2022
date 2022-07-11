5e8cb85154eaaeca5c35e07277bbf1d9f3500fe6
Yuhan Chang <hannahchang2000@gmail.com>
2022-07-10 23:10:53 -0700

    filter user searched class

--- portfolio/src/main/java/com/google/sps/data/ClassData.java
+++ portfolio/src/main/java/com/google/sps/data/ClassData.java
@@ -1,15 +1,19 @@
 package com.google.sps.data;
 
 public class ClassData{
-    private String schoolName, department, professor, className;
-    private Long classYear,timestamp;
-    public ClassData(String schoolName, String department, String professor, Long classYear, String className, Long timestamp){
+    public String schoolName, department, professor, className, semester;
+    public Long classYear,timestamp;
+    public ClassData(String schoolName, String department, String professor, String semester, Long classYear, String className){
         this.schoolName = schoolName;
         this.department = department;
         this.professor = professor;
         this.className = className;
         this.classYear = classYear;
-        this.timestamp = timestamp;
+        //this.timestamp = timestamp;
+        this.semester = semester;
     }
+
+    
+
 
 }
--- portfolio/src/main/java/com/google/sps/servlets/SearchClassServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/SearchClassServlet.java
@@ -25,34 +25,66 @@ import java.util.List;
 @WebServlet("/search-class")
 public class SearchClassServlet extends HttpServlet {
     @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
       Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
       Query<Entity> query =
           Query.newEntityQueryBuilder().setKind("Class").setOrderBy(OrderBy.desc("timestamp")).build();
       QueryResults<Entity> results = datastore.run(query);
+      
+      //get the filter option from user
+      String schoolName = request.getParameter("schoolName");
+      String department = request.getParameter("department");
+      String professor = request.getParameter("professor");
+      String semester = request.getParameter("classSemester");
+      String className = request.getParameter("className");
+      Long classYear = Long.valueOf(request.getParameter("classYear"));
+
+      //create a new classData
+      ClassData userInputData = new ClassData(schoolName, department, professor, semester, classYear, className);
+      
+      //TODO: test if any not required field is empty
+      
+      List<ClassData> classesResult = filterResult(results, userInputData);
+      
+      String formsJson = convertToJsonByGson(classesResult);
   
+      response.setContentType("application/json;");
+      response.getWriter().println(formsJson);
+      
+    }
+
+    public List filterResult(QueryResults<Entity> results, ClassData usrInputData ){
       List<ClassData> classes = new ArrayList<ClassData>();
       while (results.hasNext()) {
         Entity entity = results.next();
   
         long id = entity.getKey().getId();
-        String schoolName = entity.getString("schoolName");
+        String school = entity.getString("schoolName");
         String department = entity.getString("department");
         String professor = entity.getString("professor");
-        // String semester = entity.getString("semester");
+        String semester = entity.getString("classSemester");
         String className = entity.getString("className");
-        Long classYear = entity.getLong("classYear");
-        Long timestamp = entity.getLong("timestamp");
+        Long year = entity.getLong("classYear");
+
+        if (usrInputData.schoolName == school){
+          if (usrInputData.department == department){
+            if (usrInputData.professor == professor){
+              if(usrInputData.semester == semester){
+                if (usrInputData.className == className){
+                  if (usrInputData.classYear == year){
+                    ClassData c = new ClassData(school, department, professor,semester, year, className);
+                    classes.add(c);
+                  }
+                }
+              }
+            }
+          }
+        }
   
-        ClassData c = new ClassData(schoolName, department, professor,classYear, className,timestamp);
-        classes.add(c);
+        
       }
-  
-      String formsJson = convertToJsonByGson(classes);
-  
-      response.setContentType("application/json;");
-      response.getWriter().println(formsJson);
       
+      return classes;
     }
 
     /**
